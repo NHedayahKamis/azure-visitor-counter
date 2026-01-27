1cfdb88789443a53d5db0d921cf2feb51bd5ad54
NHedayahKamis <hedayahkamis96@gmail.com>
2026-01-27 13:01:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from azure.cosmos import CosmosClient
 
 app = Flask(_name_)
+
 url = os.environ.get("COSMOS_ENDPOINT")
 key = os.environ.get("AZURE_COSMOS_CONNECTION_STRING")
 client = CosmosClient(url,credential=key)
@@ -11,9 +12,9 @@ container = database.get_container_client("v_container")
 
 @app.route("/")
 def fello():
-  return "<h1>Hello! This is my Project 2 Website.</h1><p>The visitor counter is coming next!</p>"
   item = container.read_item(item="1", partition_key="1")
   item['count'] += 1
   container.upsert_item(item)
+  
   return f"<h1>Welcome!</h1><p>You are visitor number: {item['count']}</p>"
 
